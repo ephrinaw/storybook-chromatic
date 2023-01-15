be6356b1f318ef38dad3d24ed7ae97344d996ab0
Ephrem <39829776+ephrinaw@users.noreply.github.com>
2023-01-15 20:25:04 +0200

    second commit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-};
+
+ import '../src/index.css';
+
+ //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+ export const parameters = {
+   actions: { argTypesRegex: '^on[A-Z].*' },
+   controls: {
+     matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/,
+     },
+   },
+ };
